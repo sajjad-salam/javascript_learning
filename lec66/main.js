a1bd56375b555a6f8558cf55e3d5c94b027408a9
sajjad-salam <sajjad.salam.teama@gmail.com>
2025-06-23 17:42:31 +0300

    اضافة الدرس 66

---
+++ lec66/main.js
@@ -0,0 +1,18 @@
+console.log(location.href); //http://127.0.0.1:5500/lec66/
+
+console.log(location.protocol); //http:
+
+console.log(location.host); //127.0.0.1:5500
+
+console.log(location.hostname); //127.0.0.1
+// اكدر اغير فقط الهوست نيم او الباث نيم
+
+console.log(location.pathname); ///lec66/
+
+console.log(location.port); //5500
+
+// اذا تريد تروح لرابط معين
+// location.assign('http://127.0.0.1:5500/lec64');
+
+// اذا تريد تروح لرابط معين بس بدون رجعة
+// location.replace('http://127.0.0.1:5500/lec64/');
