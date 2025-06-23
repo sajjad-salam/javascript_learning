475747eded5f3bc3cbabb08b78649ee40572c0f7
sajjad-salam <sajjad.salam.teama@gmail.com>
2025-06-23 17:14:45 +0300

    اضافة الدرس 64 درس السكرين

---
+++ lec64/main.js
@@ -0,0 +1,9 @@
+// درس السكرين
+
+console.log(screen.width); // عرض الشاشة
+console.log(screen.height); // طول الشاشة
+
+console.log(screen.availHeight); // الطول
+console.log(screen.availWidth);
+
+console.log(screen.orientation.type); //landscape-primary
